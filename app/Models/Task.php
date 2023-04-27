e3882b1935a123321517731e3fbd1bc57a86ed32
Abiel Magu <abielmagu.dev@gmail.com>
2023-04-27 01:49:30 -0500

    Factorize attributes of Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
+
+
+    // Attributes
+
+    public function getFullScheduledAttribute()
+    {
+        return sprintf('%s %s', $this->scheduled_date, $this->scheduled_time);
+    }
+
+    public function getFullScheduledHumanAttribute()
+    {
+        return Carbon::parse( $this->full_scheduled )->toDayDateTimeString();
+    }
+
+    public function getScheduledDateHumanAttribute()
+    {
+        return Carbon::parse($this->scheduled_date)->format('D, M d, Y');
+    }
+
+    public function getScheduledTimeHumanAttribute()
+    {
+        return Carbon::parse($this->scheduled_time)->format('h:i A');
+    }
+
+
+    // Relationships
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
 }
